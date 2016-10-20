8978cf49f8128ba4f3154d5c9f10b9c2176599ef
PurpleRaccoon <jkemery@capital.edu>
2016-10-20 09:15:35 -0400

    added CalendarConnection class to project

---
+++ Calendar Checklist/Calendar Checklist/CalendarConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Outlook;
+
+namespace Calendar_Checklist
+{
+    public class CallendarConnection
+    {
+        public CallendarConnection()
+        {
+        }
+        /*
+        public static Array[] getEvents()
+        {
+        }
+        */
+    }
+
+}
